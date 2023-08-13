895ce5c36d02f759c358050c96ab4b54f1e1de62
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-08-13 10:37:24 -0400

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Suspense, lazy } from "react";
 import { Route } from "wouter";
 
 const UsersPage = lazy(() => import("./pages/index"));
+const TodosPage = lazy(() => import("./pages/todos"));
 
 export default function App() {
   return (
     <main>
       <Suspense fallback="loading...">
         <Route path="/" component={UsersPage} />
+        <Route path="/todos" component={TodosPage} />
       </Suspense>
     </main>
   );
